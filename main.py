51bbff14bfae9e6e1e027827d742b8c964c57084
devanshichavda <46798129+devanshichavda@users.noreply.github.com>
2025-03-10 23:22:48 +0000

    - added decrypt function to decrypt encrypted file using the same key - added user input functionality to choose options between encrypt, decrypt, and exit

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 def Encrypt(filename, key):
-  file = open(filename, "rb") #rb for encoding issues
+  file = open(filename, "rb") #rb for encoding issues so files other than text can be used
   data = file.read()
-  print(data)
+  file.close()
+
+  #converts file to bytearray
+  data = bytearray(data)
+  for index, value in enumerate(data):
+    #whatever value at exclusive or (XOR) for key passed in
+    data[index] = value ^ key
+
+  #make copy of file that will be encrypted version
+  file = open("E-" + filename, "wb")
+  file.write(data)
+  file.close()
+
+#decrypts encrypted file with same key, creating new unencrypted version of file
+def Decrypt(filename, key):
+  file = open(filename, "rb")
+  data = file.read()
   file.close()
 
   data = bytearray(data)
   for index, value in enumerate(data):
-    #print(f"index: {index} value: {value}")
-    #whatever value at exclusive or for key passed in
     data[index] = value ^ key
 
-  #make copy of file
-  file = open("D-" + filename, "wb")
+  file = open(filename, "wb")
   file.write(data)
   file.close()
 
-key = int(input("Ask for a key between 1 and 255: "))
-filename = input("Enter a file name: ")
-Encrypt(filename, key)
+choice = ""
+while choice != "3":
+  print("Select input your option (1-3): ")
+  print("1. Encrypt File")
+  print("2. Decrypt File")
+  print("3. Exit")
+  choice = input()
+  if choice == "1" or choice == "2":
+    key = int(input("Enter a key (1-255): \n"))
+    filename = input("Enter filename (ex: example.py): \n")
+    if choice == "1":
+      Encrypt(filename, key)
+    if choice == "2":
+      Encrypt(filename, key)
+  else:
+    if choice != "3":
+      print("Invalid choice. Please input a valid choice (1-3).\n")
+    else:
+      break
+
+
 
